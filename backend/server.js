1d88703c44ead091113796b23bc04cde2a48899a
ayayaderrick <ayayaderrick@protonmail.com>
2025-05-02 13:54:22 +0300

    public static middleware

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 app.use(cors());
 
